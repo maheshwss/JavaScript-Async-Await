b8711dc65481005f6332549ad56f054b0820baac
maheshwss <maheshy@wharfstreetstrategies.com>
2022-10-07 15:35:11 +0530

    js async await

---
+++ main.js
@@ -0,0 +1,40 @@
+function makeRequest(location) {
+    return new Promise((resolve, reject) => {
+        console.log(`Making Request to ${location}`);
+        if (location === "Google") {
+            resolve('Google says hi');
+        } else {
+            reject('We can only talk to google');
+        }
+    });
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing response');
+        resolve(`Extra Information + ${response}`);
+    });
+}
+
+// makeRequest('Facebook').then(response => {
+//     console.log('Response received');
+//     return processRequest(response);
+// }).then(processedResponse => {
+//     console.log(processedResponse);
+// }).catch(err => {
+//     console.log(err);
+// })
+
+// Async await
+async function doWork() {
+    try {
+        const response = await makeRequest('Google');
+        console.log('Response Received');
+        const processedResponse = await processRequest(response);
+        console.log(processedResponse);
+    } catch(err){
+        console.log(err)
+    }
+}
+
+doWork();
